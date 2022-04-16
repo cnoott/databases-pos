5cd5c48f065c02cda378c2b74dca987ba72a6c23
Jason <jasonsuerte23@gmail.com>
2022-04-16 02:07:37 -0500

    vendor pages

--- databases-pos-vs/databases-pos-vs/Controllers/VendorController.cs
+++ databases-pos-vs/databases-pos-vs/Controllers/VendorController.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Data.SqlClient;
 using System.Data;
 using MySql.Data.MySqlClient;
-using System.Web;
+
 namespace databseApp.Controllers
 {
     public class VendorController : Controller
@@ -43,13 +43,92 @@ namespace databseApp.Controllers
                 vendors = new MySqlDataAdapter(sql, sqlConnection);
                 MySqlCommandBuilder cb = new MySqlCommandBuilder(vendors);
                 vendors.Fill(dtbl);
-                
             }
+
             return View(dtbl);
         }
 
+        public IActionResult Create()
+        {
+            VendorViewModel vendorVM = new VendorViewModel();
+            return View(vendorVM);
+        }
         
-
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("VendorName, Location, Manager")] VendorViewModel vendorViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                using (MySqlConnection sqlConnection = new MySqlConnection(_configuration.GetConnectionString("DevConnection")))
+                {
+                    sqlConnection.Open();
+                    MySqlCommand sqlCmd = new MySqlCommand("AddVendor", sqlConnection);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@name_", vendorViewModel.VendorName);
+                    sqlCmd.Parameters.AddWithValue("@manager_", vendorViewModel.Location);
+                    sqlCmd.Parameters.AddWithValue("@location_", vendorViewModel.Manager);
+                    sqlCmd.ExecuteNonQuery();
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vendorViewModel);
+        }
      
+        public IActionResult Edit(int id)
+        {
+            VendorViewModel vendor = FetchVendor(id);
+            return View(vendor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([Bind("VendorID, VendorName, Location, Manager")] VendorViewModel vendorViewModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+                using (MySqlConnection sqlConnection = new MySqlConnection(_configuration.GetConnectionString("DevConnection")))
+                {
+                    
+                    sqlConnection.Open();
+                    MySqlCommand sqlCmd;
+
+                    sqlCmd = new MySqlCommand("EditVendor", sqlConnection);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@id_", vendorViewModel.VendorID);
+                    sqlCmd.Parameters.AddWithValue("@name_", vendorViewModel.VendorName);
+                    sqlCmd.Parameters.AddWithValue("@manager_", vendorViewModel.Manager);
+                    sqlCmd.Parameters.AddWithValue("@location_", vendorViewModel.Location);
+
+                    sqlCmd.ExecuteNonQuery();
+                    sqlConnection.Close();
+                }
+            }
+            return RedirectToAction("Index");
+        } 
+
+
+        public VendorViewModel FetchVendor(int id)
+        {
+            VendorViewModel vendorViewModel = new VendorViewModel();
+            using (MySqlConnection sqlConnection = new MySqlConnection(_configuration.GetConnectionString("DevConnection")))
+            {
+                MySqlDataAdapter daProducts;
+                DataTable dtbl = new DataTable();
+
+                sqlConnection.Open();
+                string sql = string.Format("SELECT * FROM Vendors WHERE '"+id+"' = vendorID");
+
+                daProducts = new MySqlDataAdapter(sql, sqlConnection);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(daProducts);
+                daProducts.Fill(dtbl);
+                vendorViewModel.VendorID = Convert.ToInt32(dtbl.Rows[0]["vendorID"].ToString());
+                vendorViewModel.VendorName = dtbl.Rows[0]["vendor_name"].ToString(); 
+                vendorViewModel.Location = dtbl.Rows[0]["location"].ToString(); 
+                vendorViewModel.Manager = dtbl.Rows[0]["manager"].ToString(); 
+            }
+            return vendorViewModel;
+        }
     }
 }
